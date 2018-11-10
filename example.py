3272c0e5ed9e2f457f6390845b8ff7f79b9bb95c
mchengg <mchengg@gmail.com>
2018-11-10 01:51:18 -0500

    first working version

--- example.py
+++ example.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import time
+import base64
 
 from src import InstaBot
 from src.check_status import check_status
@@ -11,11 +12,11 @@ from src.unfollow_protocol import unfollow_protocol
 
 bot = InstaBot(
     login="between_boundaries",
-    password="#1Password",
-    like_per_day=1000,
-    comments_per_day=0,
-    tag_list=['follow4follow', 'f4f', 'cute', 'l:212999109'],
-    tag_blacklist=['rain', 'thunderstorm'],
+    password=base64.b64decode(b'cmVnaVNwZWs3').decode('utf-8'),
+    like_per_day=250,
+    comments_per_day=200,
+    tag_list=['naturephotography','urbanphotography','scuba'],
+    tag_blacklist=['nsfw'], 
     user_blacklist={},
     max_like_for_one_tag=50,
     follow_per_day=300,
@@ -27,29 +28,12 @@ bot = InstaBot(
     proxy='',
     # List of list of words, each of which will be used to generate comment
     # For example: "This shot feels wow!"
-    comment_list=[["this", "the", "your"],
-                  ["photo", "picture", "pic", "shot", "snapshot"],
-                  ["is", "looks", "feels", "is really"],
-                  ["great", "super", "good", "very good", "good", "wow",
-                   "WOW", "cool", "GREAT","magnificent", "magical",
-                   "very cool", "stylish", "beautiful", "so beautiful",
-                   "so stylish", "so professional", "lovely",
-                   "so lovely", "very lovely", "glorious","so glorious",
-                   "very glorious", "adorable", "excellent", "amazing"],
-                  [".", "..", "...", "!", "!!", "!!!"]],
+    comment_list=[["Wow!","Epic!"]],
     # Use unwanted_username_list to block usernames containing a string
     ## Will do partial matches; i.e. 'mozart' will block 'legend_mozart'
     ### 'free_followers' will be blocked because it contains 'free'
-    unwanted_username_list=[
-        'second', 'stuff', 'art', 'project', 'love', 'life', 'food', 'blog',
-        'free', 'keren', 'photo', 'graphy', 'indo', 'travel', 'art', 'shop',
-        'store', 'sex', 'toko', 'jual', 'online', 'murah', 'jam', 'kaos',
-        'case', 'baju', 'fashion', 'corp', 'tas', 'butik', 'grosir', 'karpet',
-        'sosis', 'salon', 'skin', 'care', 'cloth', 'tech', 'rental', 'kamera',
-        'beauty', 'express', 'kredit', 'collection', 'impor', 'preloved',
-        'follow', 'follower', 'gain', '.id', '_id', 'bags'
-    ],
-    unfollow_whitelist=['example_user_1', 'example_user_2'])
+    unwanted_username_list=[],
+    unfollow_whitelist=[])
 while True:
 
     #print("# MODE 0 = ORIGINAL MODE BY LEVPASHA")
